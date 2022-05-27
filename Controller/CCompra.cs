69fed5e298f8576143253d46370ac758207f3d68
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-05-26 22:53:55 -0300

    Adicionando menu de compras para administrador

--- Controller/CCompra.cs
+++ Controller/CCompra.cs
@@ -28,5 +28,43 @@ namespace ShopBr.Controller
             }
             return "Erro ao adicionar compra";
         }
+        public void RemoveByIds(string clienteid, Guid produtoid)
+        {
+            Cmd.Parameters.Clear();
+            Cmd.CommandText = "DELETE FROM Compra WHERE ClientId = @Id AND ProdutoId = @Produto";
+            Cmd.Parameters.AddWithValue("@id",clienteid);
+            Cmd.Parameters.AddWithValue("@Produto",produtoid);
+            Cmd.Connection = conectar();
+            Cmd.ExecuteNonQuery();
+            desconectar();
+        }
+
+        public List<Compra> Get()
+        {
+            Cmd.Parameters.Clear();
+            var compras= new List<Compra>();
+            Cmd.CommandText = "SELECT ClienteId, ProdutoId, CorreioId, Valor, Quantidade, TipoPagamento FROM Compra ";
+            Cmd.Connection = conectar();
+
+            using(SqlDataReader reader = Cmd.ExecuteReader())
+            {
+                while(reader.Read()){
+                        compras.Add(new Compra((string)reader["ClienteId"], (Guid)reader["ProdutoId"], (int)reader["Quantidade"], (string)reader["TipoPagamento"], (Guid)reader["CorreioId"], (decimal)reader["valor"]));
+                }
+            }
+            desconectar();
+            return compras ;
+        }
+
+        public void RemoveById(string id)
+        {
+            Cmd.Parameters.Clear();
+            Cmd.CommandText = "DELETE FROM Compra WHERE ClienteId = @Id";
+            Cmd.Parameters.AddWithValue("@id",id);
+            Cmd.Connection = conectar();
+            Cmd.ExecuteNonQuery();
+            desconectar();
+        } 
+
     }
 }
---
+++ View/AdmCompras.cs
@@ -0,0 +1,37 @@
+using ShopBr.Controller;
+using ShopBr.Model;
+using UserSolicitor;
+
+namespace ShopBr.View
+{
+    public class AdmCompras
+    {
+        public AdmCompras()
+        {
+            Gerenciador = new CCompra();
+        }
+        public CCompra Gerenciador { get; set; }
+        public void Menu()
+        {
+            Console.Clear();
+            int contador = 0;
+            List<Compra> compras = Gerenciador.Get();
+            foreach(Compra compra in compras)
+            {
+                Console.WriteLine($"{contador} {compra}}");
+                contador+=1;
+            }
+            if(contador>0)
+            {
+                Console.WriteLine("\n\n");
+                Console.WriteLine("1- Para excluir uma avaliacao");
+                Console.WriteLine("2- Para sair");
+                byte option = Solicitor.GetByteInterval(1,2);
+                if(option == 1){
+                    Console.WriteLine("Insira o numero da compra que deseja remover");
+                    int remover = Solicitor.GetIntInterval(0, contador-1);
+                    Gerenciador.RemoveByIds(compras[remover].Cpf, compras[remover].IdProduto);
+                }   
+            }
+    }
+}
